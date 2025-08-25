368b5fa16630a6ad0063f813951924c47cf0d3d4
Majd Alhader <mjd0216623@ju.edu.jo>
2025-08-25 08:39:58 +0300

    before exception handling

---
+++ book-network/src/main/java/com/alhader/book/BookNetworkApiApplication.java
@@ -0,0 +1,32 @@
+package com.alhader.book;
+
+import com.alhader.book.role.Role;
+import com.alhader.book.role.RoleRepository;
+import com.alhader.book.user.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
+public class BookNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkApiApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(RoleRepository roleRepository) {
+
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
+	}
+}
